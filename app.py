730c10e79c1c672ffcfef04013e056f31727ee02
aungthuhtet501289 <125335230+aungthuhtet501289@users.noreply.github.com>
2023-08-22 08:50:12 +0630

    Add files via upload

---
+++ app.py
@@ -0,0 +1,22 @@
+import pickle
+from flask import Flask ,render_template,request,redirect, url_for
+model=pickle.load(open('lr_classifier.pkl','rb'))
+
+app=Flask(__name__) 
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    if(request.method=='POST'):
+        sepalLength=float(request.form['Sepallength'])
+        sepalWidth=float(request.form['Sepalwidth'])
+        petallength=float(request.form['Petallength'])
+        petalwidth=float(request.form['Petalwidth'])
+        pred=model.predict([[sepalLength,sepalWidth,petallength,petalwidth]])
+        return render_template('index.html',result=pred[0])
+
+if __name__=='__main__':
+    app.run(debug=True)
